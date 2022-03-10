18ade36c0d85a3dc1555264ffe15b088b10594ff
Luan Rafael <luanraffaell@hotmail.com>
2022-03-10 14:59:42 -0300

    Estado e Cidade

--- src/main/java/com/estudandospring/course/config/TestConfig.java
+++ src/main/java/com/estudandospring/course/config/TestConfig.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.estudandospring.course.domain.Categoria;
+import com.estudandospring.course.domain.Cidade;
+import com.estudandospring.course.domain.Estado;
 import com.estudandospring.course.domain.Produto;
 import com.estudandospring.course.repositories.CategoriaRepository;
+import com.estudandospring.course.repositories.CidadeRepository;
+import com.estudandospring.course.repositories.EstadoRepository;
 import com.estudandospring.course.repositories.ProdutoRepository;
 
 @Configuration
@@ -21,6 +25,12 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -38,8 +48,23 @@ public class TestConfig implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 		
 	}
 
